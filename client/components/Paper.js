17afa018453498bd427f4d64728e4252ccf88ab2
DANNIEL <donghoon149@gmail.com>
2020-07-13 22:45:16 +0900

    fix bugs

--- client/components/Paper.js
+++ client/components/Paper.js
@@ -1,16 +1,11 @@
 import React from "react";
 import "../styles/paper.scss";
-import { WiredCard, WiredButton } from "wired-elements";
 
 const Paper = () => {
   return (
-    <>
-      <wired-card elevation="4">
-        <p>
-          사랑한다. 동훈아! 생일 진짜 축하하고 앞으로도 좋은 친구로 지내자!!!!
-        </p>
-      </wired-card>
-    </>
+    <div className="paper">
+      사랑한다. 동훈아! 생일 진짜 축하하고 앞으로도 좋은 친구로 지내자!!!!
+    </div>
   );
 };
 
--- client/pages/write.js
+++ client/pages/write.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Link from "next/link";
 import Paper from "../components/Paper";
-import { WiredCard, WiredButton } from "wired-elements";
+
 const Write = () => {
   return (
     <div>
       <h1>링크가 생성되었습니다. 친구들에게 공유하세요!</h1>
       <h3>https://rollingpaper/?donghoonryoo</h3>
       <Link href="/write">
-        <wired-button elevation="3">공유하기</wired-button>
+        <button>복사</button>
       </Link>
       <br />
       <Link href="/">
-        <wired-button elevation="3">돌아가기</wired-button>
+        <button>돌아가기</button>
       </Link>
       <Paper />
       <Paper />
